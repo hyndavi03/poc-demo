7d19bdbb9dc50b26b877dc51fc4a52e1bbb0ef85
Hyndavi <venkatahyndavi@gmail.com>
2023-11-20 09:56:30 +0530

    modified

--- src/main/java/com/example/main.java
+++ src/main/java/com/example/main.java
@@ -2,7 +2,8 @@
 package com.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public String handleRequest(String input) {
+        return "Hello, " + input;
     }
 }
+
